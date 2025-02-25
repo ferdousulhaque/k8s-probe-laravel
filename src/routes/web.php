c2a2b605c3fd6b004245313062b4565dff936cb9
A S Md Ferdousul Haque <ferdousul.haque@gmail.com>
2025-02-25 13:49:23 +0600

    Fixed the Package Detection

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'k8s-probe'], function () {
-    Route::get('/liveness', [\Ferdous\K8s\Controller\HealthCheckController::class, 'readiness']);
+    Route::get('/liveness', [Ferdous\K8s\Controller\HealthCheckController::class, 'readiness']);
     Route::get('/readiness', [Ferdous\K8s\Controller\HealthCheckController::class, 'liveness']);
     Route::get('/startup', [Ferdous\K8s\Controller\HealthCheckController::class, 'startup']);
 });
