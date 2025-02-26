183986832338d5fac682a8ab382131266d1d0d05
A S Md Ferdousul Haque <ferdousul.haque@gmail.com>
2025-02-26 08:12:47 +0600

    Fixed the Typo

--- config/k8s-health.php
+++ config/k8s-health.php
@@ -4,8 +4,8 @@ return [
     'checkpoints' => [
         'live' => [],
         'ready' => [
-                new Ferdous\K8s\Checkpoint\Cache(),
-                new Ferdous\K8s\Checkpoint\Database()
+                new Ferdous\K8s\CheckPoint\Cache(),
+                new Ferdous\K8s\CheckPoint\Database()
         ],
         'startup' => [],
     ]
